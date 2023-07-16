88c0e47d6ff663aeaa6e473a0fb528133977319a
Hyun Jung KANG <clinguist.hjkang@gmail.com>
2023-07-16 14:07:08 +0900

    modify code

--- exercise/parse_xls_files.py
+++ exercise/parse_xls_files.py
@@ -24,7 +24,6 @@ def parse_file(datafile):
     workbook = xlrd.open_workbook(datafile)
     sheet = workbook.sheet_by_index(0)
 
-    ### example on how you can get the data
     sheet_data = [[sheet.cell_value(r, col) for col in range(sheet.ncols)] for r in range(sheet.nrows)]
     print("Get a list of values in column 1('COAST'), from all rows (exclude header)")
     COAST_list = sheet.col_values(1, start_rowx=1, end_rowx=None)   
